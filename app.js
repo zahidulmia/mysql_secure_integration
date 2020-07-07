3240eb55505695c07d597f2b447bbcdf27f289bb
Zahidul Haque <zahidulmia@users.noreply.github.com>
2020-07-07 22:49:06 +0300

    final

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mysql = require("mysql");
 const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
 dotenv.config({
   path: "./.env"
 });
@@ -21,6 +22,7 @@ app.set("view engine", "hbs");
 
 app.use(express.urlencoded({extended :false}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 
--- controllers/auth.js
+++ controllers/auth.js
@@ -9,7 +9,52 @@ const db = mysql.createConnection({
   database: process.env.DATABASE
 });
 
+exports.login = async (req, res) => {
+  try {
+    const {
+      email,
+      password
+     } = req.body;
 
+    if (!email || !password) {
+      return res.status(400).render("login", {
+        massage: "please provide email and password"
+
+      })
+    }
+db.query("SELECT * FROM user WHERE email =?",[email], async(error,result)=>{
+
+if(!result || !(await bcrypt.compare(password,result[0].password))){
+  res.status(400).render("login", {
+    massage: " incorrect"
+})
+}else{
+const id = result[0].id;
+const token = jwt.sign({id},process.snv.jwt_secret,{
+expiresIn : process.env.jwt_expires_in
+
+});
+console.log(token);
+const cookieOtion={
+  expires: new Date(
+  Date.now() + process.env.jwt_cookie_expires * 24 *60 *60
+
+),
+httpOnly : true
+}
+res.cookie("jwt",token,cookieOption);
+res.status(200).redirect("/");
+}
+
+} )
+
+}
+
+  catch (error) {
+console.log(error);
+  }
+
+}
 
 
 exports.register = (req, res) => {
